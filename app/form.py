283f9cdeaef2fd9136a416db380a15b83696e3b8
bistudio <basswrks@hotmail.com>
2020-07-19 17:16:59 +0100

    implemented d3 general update pattern

--- app/form.py
+++ app/form.py
@@ -9,10 +9,17 @@ def country_select_query():
     return Country.query
 
 
+def default_select():
+    from app.models import Country
+    default_country = Country.query.filter_by(country_code="BR").first()
+    return default_country
+
+
 class CountryForm(FlaskForm):
     country_name = QuerySelectField('Country', query_factory=country_select_query
                                     , validators=[DataRequired()], allow_blank=True, blank_text='Select'
-                                    , get_pk=lambda a: a.country_code, get_label='country_name')
+                                    , get_pk=lambda a: a.country_code, get_label='country_name'
+                                    , default=default_select())
 
     def __str__(self):
         return f'{self.country_code}, {self.country_name}'
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, request, url_for
 from app.models import Country, Covid19HistoricalData
 # import app.read_data as data_reader
 from app.form import CountryForm
+from app.models import Country
 import json
 
 
@@ -10,9 +11,28 @@ import json
 @app.route('/home')
 def home():
     form = CountryForm()
+
+    sc = Country.query.filter_by(id=form.country_name.data.id).first()
+    sc_code = form.country_name.data.country_code
+
     with open('./app/data/covid.json', 'r') as f:
         covid_data = f.read()
 
     obj = json.loads(covid_data)
 
-    return render_template('d3_dashboard.html', form=form, covid_data=obj, title="Covid-19 Dashboard")
+    return render_template('d3_dashboard.html', form=form, covid_data=obj, sc_code=sc_code, title="Covid-19 Dashboard")
+
+
+@app.route('/d3_enter')
+def d3_enter():
+    return render_template('general_update_pattern.html')
+
+
+@app.route('/d3_exit')
+def d3_exit():
+    return render_template('d3_exit.html')
+
+
+@app.route('/d3_update')
+def d3_update():
+    return render_template('d3_update.html')
